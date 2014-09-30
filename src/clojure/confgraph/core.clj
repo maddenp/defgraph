ba36cf453da05c0ddb956c42a45b98e68b7ae84b
Paul Madden <maddenp@colorado.edu>
2014-09-30 14:26:32 -0600

    edges -> raw-edges, filtered-edges -> edges

--- src/clojure/confgraph/core.clj
+++ src/clojure/confgraph/core.clj
@@ -4,22 +4,22 @@
   (:import java.io.File)
   (:import org.yaml.snakeyaml.Yaml))
 
-(def yaml     (Yaml. (ExtendedConstructor.)))
-(def extends  (memoize (fn [d] (.get (.load yaml (slurp (.getPath d))) "ddts_extends"))))
-(def defs     (for [x (.listFiles (File. "defs/runs")) :when (.isFile x)] x))
-(def vertices (for [x defs] (.getName x)))
-(def edges    (zipmap vertices (for [x defs] (extends x))))
+(def yaml      (Yaml. (ExtendedConstructor.)))
+(def extends   (memoize (fn [d] (.get (.load yaml (slurp (.getPath d))) "ddts_extends"))))
+(def defs      (for [x (.listFiles (File. "defs/runs")) :when (.isFile x)] x))
+(def vertices  (for [x defs] (.getName x)))
+(def raw-edges (zipmap vertices (for [x defs] (extends x))))
 
 (def rootpath
   (memoize (fn [src]
-             (let [dst (edges src)]
+             (let [dst (raw-edges src)]
                (if (nil? dst)
                  {}
                  (conj {src dst} (rootpath dst)))))))
 
-(defn filtered-edges [prefix]
+(defn edges [prefix]
   (let [re (re-pattern (str prefix ".*"))]
-    (into {} (for [[src dst] edges :when (re-matches re src)] (rootpath src)))))
+    (into {} (for [[src dst] raw-edges :when (re-matches re src)] (rootpath src)))))
 
 (defn usage []
   (println "Supply at most a single filtering prefix.")
@@ -28,5 +28,5 @@
 (defn -main [& args]
   (alter-var-root #'*read-eval* (constantly false))
   (if (> (count args) 1) (usage))
-  (let [fe (filtered-edges (first args))]
+  (let [fe (edges (first args))]
     (prn fe)))
