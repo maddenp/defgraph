fa6ce9e8d4ef1576b1c8660a933d7fca82cf9d4f
Paul Madden <maddenp@colorado.edu>
2014-10-02 13:57:32 -0600

    add local maven repo with jgraphx.jar

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject defgraph "1"
   :aot [defgraph.core]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[local/jgraphx "3.0.1.1"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.yaml/snakeyaml "1.12"]]
   :java-source-paths ["src/java"]
   :main defgraph.core
-  :source-paths ["src/clojure"]
-  )
+  :repositories {"local" "file:maven"}
+  :source-paths ["src/clojure"])
